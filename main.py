78d03e82a7b16844d00edad4f88eca3e3e725a7e
bovle <frederik@live.dk>
2022-04-07 19:00:10 +0200

    added hand type to json

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import json
 from poker_game_runner.runner import play_tournament_table, BlindScheduleElement
-from poker_game_runner.bots import randombot
-import mybot
+from poker_game_runner.bots import randombot, fbl_bot
 
 
-bots = [mybot, mybot, randombot, randombot, mybot, mybot, randombot, randombot, mybot, mybot]
+bots = [fbl_bot, fbl_bot, randombot, randombot, fbl_bot, fbl_bot, randombot, randombot, fbl_bot, fbl_bot]
 bot_instances = [b.Bot() for b in bots]
 
-res, details = play_tournament_table(bot_instances, 1000, tuple(BlindScheduleElement(i*10, i*5,i*10,0) for i in range(1,100)) +
-                                                           (BlindScheduleElement(-1, 500,1000,0), ))
+res, details = play_tournament_table(bot_instances, 500, 
+                (BlindScheduleElement(20, 5,10,0),
+                BlindScheduleElement(40, 10,20,0),     
+                BlindScheduleElement(60, 15,30,0),
+                BlindScheduleElement(80, 20,40,0),
+                BlindScheduleElement(100, 25,50,0),
+                BlindScheduleElement(110, 35,70,0),
+                BlindScheduleElement(120, 50,100,0),
+                BlindScheduleElement(130, 75,150,0),
+                BlindScheduleElement(140, 100,200,0),
+                BlindScheduleElement(150, 150,300,0),
+                BlindScheduleElement(160, 200,400,0),
+                BlindScheduleElement(170, 300,600,0),
+                BlindScheduleElement(180, 400,800,0),
+                BlindScheduleElement(190, 500,1000,0),
+                BlindScheduleElement(200, 750,1500,0),
+                BlindScheduleElement(210, 1000,2000,0),
+                BlindScheduleElement(220, 1500,3000,0),
+                BlindScheduleElement(230, 2000,4000,0),
+                BlindScheduleElement(250, 3000,6000,0),
+                BlindScheduleElement(-1, 4000,8000,0)))
 print(res)
-print(len(details))
+
+with open('output.json', 'w') as file:
+    json.dump(details, file)
+
+
+
