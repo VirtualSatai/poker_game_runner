bf31a4acd82c12fa567c7c2996320cf3f17e749f
bovle <frederik@live.dk>
2022-04-10 20:14:59 +0200

    changed bot name

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import json
 from poker_game_runner.runner import play_tournament_table, BlindScheduleElement
-from poker_game_runner.bots import randombot, fbl_bot, foldBot, callBot
+from poker_game_runner.bots import example_bot, randombot, foldBot, callBot
 
 
-bots = [fbl_bot, foldBot, fbl_bot, fbl_bot, fbl_bot, callBot, randombot, foldBot, randombot, randombot]
+bots = [example_bot, example_bot, example_bot, example_bot, example_bot, example_bot, example_bot, example_bot, example_bot, example_bot]
 bot_instances = [b.Bot() for b in bots]
 #data = [{'name': b.get_name(), 'wins': 0} for b in bot_instances]
 #for i in range(20):
 #    res, _ = play_tournament_table(bot_instances, 1000)
 #    data[res[0]['id']]['wins'] += 1
 
-res, details = play_tournament_table(bot_instances, 1000)
-print(res)
+data, details = play_tournament_table(bot_instances, 1000, console_output=True)
+print(len(details))
+print(data)
 
-with open('output.json', 'w') as file:
-    json.dump(details, file)
 
 
 
